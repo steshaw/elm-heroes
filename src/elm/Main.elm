2580dcce9c385514f7bc93776449a1f9b6cb3431
Steven Shaw <steven@steshaw.org>
2016-12-06 13:25:19 +1000

    Editing Our Hero

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -11,35 +11,45 @@ model = 0
 
 -- View
 
-type alias Hero =
-  { id: Int
-  , name: String
-  }
+type alias Hero = {
+  id: Int,
+  name: String
+ }
 
+title : String
 title = "Tour of Heroes"
-hero =
-  { id = 1
-  , name = "Windstorm"
-  }
 
-renderHero hero =
-  [ h2 [] [(text (hero.name ++ " details!"))]
-  , div [class "row"]
-      [ label [class "col-xs-2"] [(text "id:")]
-      , span [class "col-xs-10"] [(text (toString hero.id))]
+hero : Hero
+hero = {
+  id = 1,
+  name = "Windstorm"
+ }
+
+renderHero : Hero -> List (Html msg)
+renderHero hero = [
+  h2 [] [(text (hero.name ++ " details!")) ],
+  div [class "form-horizontal"] [
+    div [class "form-group"] [
+      label [for "id", class "col-sm-2 control-label"] [(text "Id")],
+      div [class "col-sm-2"] [
+        input [id "id", class "form-control", disabled True, value (toString hero.id)] []
       ]
-  , div [class "row"]
-      [ label [class "col-xs-2"] [(text "name:")]
-      , span [class "col-xs-10"] [(text hero.name)]
+    ],
+    div [class "form-group"] [
+      label [for "name", class "control-label col-sm-2"] [(text "Name")],
+      div [class "col-sm-6"] [
+        input [id "name", class "form-control", value hero.name] []
       ]
+    ]
   ]
+ ]
 
 view : Model -> Html Msg
 view model =
-  div [class "container"]
-    [ div [ class "jumbotron" ]
-        ([h1 [class "main"] [(text title)]] ++ renderHero hero)
-    ]
+  div [class "container"] [
+    div [class "jumbotron"]
+      ([h1 [class "main"] [(text title)]] ++ renderHero hero)
+  ]
 
 -- Update
 
