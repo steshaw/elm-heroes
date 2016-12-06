97695d41360390bf0413b0cf2c2d3126e499229e
Steven Shaw <steven@steshaw.org>
2016-12-06 12:37:23 +1000

    Hero Editor - Windstorm display

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -4,22 +4,41 @@ import Html.Events exposing ( onClick )
 
 import Components.Hello exposing ( hello )
 
--- App
-
-main : Program Never Int Msg
-main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
-
 -- Model
 
 type alias Model = Int
 
 model : number
 model = 0
+
+-- View
+
+title = "Tour of Heroes"
+hero = "Windstorm"
+
+heroes =
+  [ h1 [] [(text title)]
+  , h2 [] [(text (hero ++ " details!"))]
+  ]
+
+view : Model -> Html Msg
+view model =
+  div [ class "container main" ] [
+    div [ class "jumbotron" ]
+      heroes
+{-
+      [ hello model
+      , p [] [ text "Elm Tour of Heroes" ]
+      , button [ class "btn btn-primary btn-lg", onClick Increment ]
+          [ span [ class "glyphicon glyphicon-star" ] []
+          , span [] [ text "FTW!" ]
+          ]
+      , br [] []
+      , br [] []
+      , img [ src "static/img/elm.jpg", style imgStyles ] []
+    ]
+-}
+  ]
 
 -- Update
 
@@ -31,26 +50,18 @@ update msg model =
     NoOp -> model
     Increment -> model + 1
 
--- View
-
-view : Model -> Html Msg
-view model =
-  div [ class "container main" ] [
-    div [ class "jumbotron" ]
-      [ hello model
-      , p [] [ text "Elm Tour of Heroes" ]
-      , button [ class "btn btn-primary btn-lg", onClick Increment ]
-          [ span [ class "glyphicon glyphicon-star" ] []
-          , span [] [ text "FTW!" ]
-          ]
-      , br [] []
-      , br [] []
-      , img [ src "static/img/elm.jpg", style imgStyles ] []
-    ]
-  ]
-
 imgStyles : List (String, String)
 imgStyles =
   [ ( "width", "33%" )
   , ( "border", "4px solid #337AB7")
   ]
+
+-- App
+
+main : Program Never Int Msg
+main =
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
